785ab58af44af486fe2f63cfa4b101c4e7fca36f
Sushil G <guptask.7@gmail.com>
2025-12-10 19:39:25 -0500

    Added Safari window component

---
+++ src/windows/Safari.jsx
@@ -0,0 +1,64 @@
+import { WindowControls } from "#components";
+import { blogPosts } from "#constants";
+import WindowWrapper from "#hoc/WindowWrapper";
+import { ChevronLeft, PanelLeft, ChevronRight, ShieldHalf, Search, Share, Copy, Plus, MoveRight } from "lucide-react";
+
+const Safari = () => {
+    return (
+        <>
+            <div id="window-header">
+                <WindowControls target="safari"/>
+
+                <PanelLeft className="ml-10 icon"/>
+
+                <div className="flex items-center gap-1 ml-5">
+                    <ChevronLeft className="icon" />
+                    <ChevronRight className="icon" />
+                </div>
+
+                <div className="flex-1 flex-center gap-3">
+                    <ShieldHalf className="icon" />
+
+                    <div className="search">
+                        <Search className="icon" />
+
+                        <input type="text" placeholder="Search or enter website name" className="flex-1" />
+
+                    </div>
+                </div>
+
+                <div className="flex items-center gap-5">
+                    <Share className="icon" />
+                    <Plus className="icon" />
+                    <Copy className="icon" />
+                </div>
+
+            </div>
+
+            <div className="blog">
+                <h2>My Portfolio Projects</h2>
+
+                <div className="space-y-8">
+                    {blogPosts.map(({id, image, title, date, link}) => (
+                        <div key={id} className="blog-post">
+                            <div className="col-span-2">
+                                <img src={image} alt={title} />
+                            </div>
+
+                            <div className="content">
+                                <p>{date}</p>
+                                <h3>{title}</h3>
+                                <a href={link} target="_blank" rel="noopener noreferrer">Check out the full site <MoveRight className="icon-hover" /> </a>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+    )
+}
+
+//leverage window wrapper hoc (high order component)
+const SafariWindow = WindowWrapper(Safari, 'safari');
+
+export default SafariWindow;
--- src/windows/index.js
+++ src/windows/index.js
@@ -1,3 +1,4 @@
 import Terminal from "#windows/Terminal";
+import Safari from "#windows/Safari";
 
-export {Terminal};
+export { Terminal, Safari};
